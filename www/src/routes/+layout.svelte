90cfdf193130f21820a870c839d8a5e4cc33f359
zbarnz <zbarnz@yahoo.com>
2023-05-18 01:15:25 -0600

    remove boilerplate

--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -1,17 +1,14 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Try <a href="https://github.com/SocexSolutions/prescient">Prescient</a> for a fun productivity solution</p>
 	</footer>
 </div>
 
